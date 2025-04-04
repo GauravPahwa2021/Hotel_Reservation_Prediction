0046721c3c7bc887704f3458b9e3b9072d1645f2
GauravPahwa2021 <gauravpahwa2020@gmail.com>
2025-04-04 17:32:41 +0530

    data preprocessing stage added

--- config/paths_config.py
+++ config/paths_config.py
@@ -8,3 +8,10 @@ RAW_DATA_DIR = "artifacts/raw"
 RAW_FILE_PATH = os.path.join(RAW_DATA_DIR, "raw_data.csv")
 TRAIN_FILE_PATH = os.path.join(RAW_DATA_DIR, "train.csv")
 TEST_FILE_PATH = os.path.join(RAW_DATA_DIR, "test.csv")
+
+
+####################  Data Preprocessing Config   #######################
+
+PROCESSED_DIR = "artifacts/processed"
+PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR,"processed_train.csv")
+PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR,"processed_test.csv")
